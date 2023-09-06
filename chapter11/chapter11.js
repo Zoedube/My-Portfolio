871a7e97ea1c76e5608471c79815d09cb21181a8
Zoedube <144275796+Zoedube@users.noreply.github.com>
2023-09-06 21:29:02 +0200

    Add files via upload

---
+++ chapter11/chapter11.js
@@ -0,0 +1,22 @@
+let people = ["Mary", "Bobby", "Judy", "Eddie", "Herbie", "Tony"];
+let otherPeople = ["Teddy", "Cathy", "G-berg", "George"];
+
+// people = people.concat(otherPeople);
+// people = people.indexOf("Eddie");
+//people = people.join(" # ");
+// people = people.lastIndexOf("Bobby");
+// people = people.pop();
+// people = people.push("Teddy");
+//people.shift();
+//people.unshift("Teddy");
+//people = people.slice(0,3);
+//people = people.sort();
+//people.splice(1,0,"Cathy");
+
+document.getElementById("peopleIKnow").innerHTML = people.toString();
+document.getElementById("peopleIKnow").innerHTML = people.valueOf();
+
+
+
+
+
